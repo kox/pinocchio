c0870b4cd3f350a481c91e95c453c434885ff8d5
kox <garsanzi@gmail.com>
2024-11-10 13:18:39 +0100

    adding new memo instruction

---
+++ programs/memo/src/instructions/mod.rs
@@ -0,0 +1,38 @@
+use core::slice::from_raw_parts;
+
+use pinocchio::{instruction::{self, AccountMeta, Instruction, Signer}, program::invoke_signed, ProgramResult};
+
+
+
+/// Memo instruction
+/// 
+/// ### Accounts
+///     None required
+/// ### Instruction Data:
+///     - UTF 8 encoded
+pub struct Memo<'a> {
+    /// Memo 
+    pub input: &'a[u8],
+}
+
+impl<'a> Memo<'a> {
+    /// Invoke the memo instruction
+    #[inline(always)]
+    pub fn invoke(&self) -> ProgramResult {
+        self.invoke_signed(&[])
+    }
+
+    pub fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
+        let instruction = Instruction {
+            program_id: &crate::ID,
+            accounts: &[],
+            data: self.input
+        };
+
+        invoke_signed(
+            &instruction,
+            &[],
+            signers,
+        )
+    }
+}
---
+++ programs/memo/src/lib.rs
@@ -0,0 +1,5 @@
+#![no_std]
+
+pub mod instructions;
+
+pinocchio_pubkey::declare_id!("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr");
